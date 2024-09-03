7716a7ef1c5c2d0e8633091d813c9b41968f1ed5
Lujain <lujainyah@gmail.com>
2024-09-03 08:34:17 +0300

    done

--- calculator.py
+++ calculator.py
@@ -3,24 +3,30 @@ def add(x, y):
 
 
 def subtract(x, y):
-    pass
+    return x-y
 
 
 def divide(x, y):
-    pass
-
+      if y == 0:
+        return "Invalid value for denominator, can't divide by 0!"
+      else:
+         return x / y
 
 def multiply(x, y):
-    pass
+    return x*y
 
 
 def square(x):
-    pass
+    return x*x
 
 
 def power(x, y):
-    pass
+    return x**y
 
 
 def sqrt(x):
-    pass
+     import math
+     if x < 0:
+       return "Square root is not defined for negative numbers."
+     else:
+       return math.sqrt(x)
